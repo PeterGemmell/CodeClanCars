d23a11fd3ad8afb94ee903244f578a77c3589afa
Peter Gemmell <petergemmell65@hotmail.com>
2020-03-26 09:08:28 +0000

    mvp complete

--- src/main/java/People/Dealer.java
+++ src/main/java/People/Dealer.java
@@ -1,21 +1,50 @@
-//package People;
-//
-//import Vehicles.Car;
-//
-//import java.util.ArrayList;
-//
-//public class Dealer extends Participant {
-//
-//    private String name;
-//    private int totalCash;
-//    private ArrayList<Car> cars;
-//
-//
-//    public Dealer(String name, int totalCash, ArrayList<Car> cars){
-//        super(name, totalCash, cars);
-//        this.name = name;
-//        this.totalCash = totalCash;
-//        this.cars = new ArrayList<Car>();
-//
-//    }
-//}
+package People;
+
+import Vehicles.Car;
+import Vehicles.Electric;
+
+import java.util.ArrayList;
+
+public class Dealer extends Participant {
+
+    private String name;
+    private int totalCash;
+    private ArrayList<Car> stockCars;
+
+
+    public Dealer(String name, int totalCash){
+        super(name, totalCash);
+        this.name = name;
+        this.totalCash = totalCash;
+        this.stockCars = new ArrayList<Car>();
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getTotalCash() {
+        return totalCash;
+    }
+
+    public ArrayList<Car> getStockCars() {
+        return stockCars;
+    }
+
+    public int stockCarsCount(){
+        return this.stockCars.size();
+    }
+
+    public void addStockCars(Car car, Electric electricCar){
+        this.stockCars.add(car);
+
+    }
+}
--- src/test/java/People/DealerTest.java
+++ src/test/java/People/DealerTest.java
@@ -1,4 +1,51 @@
 package People;
 
+import Components.Engine;
+import Components.Gearbox;
+import Vehicles.Electric;
+import Vehicles.Petrol;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DealerTest {
+
+    Dealer dealer;
+    Petrol petrolCar;
+    Electric electricCar;
+    Engine engine;
+    Gearbox gearbox;
+
+    @Before
+    public void before(){
+        dealer = new Dealer("CodeCars", 200000);
+        petrolCar = new Petrol("Toyota", "supra", engine, gearbox, 50000);
+        electricCar = new Electric("Nissan", "leaf", engine, gearbox, 20000);
+        petrolCar = new Petrol("BMW", "M3", engine, gearbox, 60000);
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("CodeCars", dealer.getName());
+    }
+
+    @Test
+    public void canGetDealerCash(){
+        assertEquals(200000, dealer.getTotalCash());
+    }
+
+    @Test
+    public void dealerGarageStartsEmpty(){
+        assertEquals(0, dealer.stockCarsCount());
+    }
+
+    @Test
+    public void currentlyStockedCars(){
+        dealer.addStockCars(petrolCar, electricCar);
+        assertEquals(3, dealer.stockCarsCount());
+    }
+
+
 }
